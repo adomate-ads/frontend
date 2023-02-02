e2c91d664526c8d3bc0a1061fcf02ae5ceede7b5
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2023-02-02 00:53:15 -0600

    Add icons

--- src/views/pages/errors/ErrorNotFound.vue
+++ src/views/pages/errors/ErrorNotFound.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full box404 table">
     <div class="w-full h-full table-row">
-      <h1 class="text-colorado-red text-center">404</h1>
+      <h1 class="text-red-500 text-center">404</h1>
       <h2>
         Oh No! You seem to have lost your way. I mean, that's not always a bad
         thing... losing your way can sometimes turn into a fun adventure.
