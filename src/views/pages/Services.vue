954a33552d42c615271cd728ef55abcb8c3c6b7f
jadenbanze <jadenbanze@tamu.edu>
2023-03-26 17:38:15 -0500

    finished services page

--- src/views/pages/Services.vue
+++ src/views/pages/Services.vue
@@ -2,7 +2,7 @@
   <div id="services">
     <div class="w-full flex place-items-center flex-col">
       <h1
-        class="text-4xl font-semibold subpixel-antialiased text-center pt-20 px-5"
+        class="text-4xl xl:text-5xl font-semibold subpixel-antialiased text-center pt-20 px-5"
       >
         Services built to help scale
         <strong class="hover:text-dark-purple transition-colors ease-in-out"
@@ -10,17 +10,15 @@
         >
         business
       </h1>
-      <p
-        class="text-center pt-2 px-5 border-b border-gray-200 mb-10 pb-5 mx-10"
-      >
+      <p class="text-center pt-2 px-5 mb-10 mx-10">
         With our automated advertising services, you can focus on what matters
         most. Like Raaj said, "Tres commas, baby."
       </p>
     </div>
   </div>
-  <div class="snap-y snap-mandatory overflow-x-hidden">
+  <div class="snap-y snap-mandatory">
     <!-- Hero 1 - Dashboard -->
-    <div class="snap-start">
+    <div class="snap-start shrink-0">
       <svg
         class="block sm:hidden w-full"
         xmlns="http://www.w3.org/2000/svg"
@@ -55,7 +53,7 @@
                 <a
                   href="#"
                   class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-dark-purple shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-                  >Get started</a
+                  >Go to dashboard</a
                 >
                 <a href="#" class="text-sm font-semibold leading-6 text-white"
                   >Learn more <span aria-hidden="true">→</span></a
@@ -66,7 +64,7 @@
               <img
                 class="w-3/4 rounded-xl hover:scale-105 transition duration-500 ease-in-out shadow-xl"
                 src="@/assets/home/banner.png"
-                alt="App screenshot"
+                alt="Dashboard Screenshot"
               />
             </div>
           </div>
@@ -84,8 +82,8 @@
         </div>
       </div>
     </div>
-    <!-- Hero 2 - Dashboard -->
-    <div class="snap-center">
+    <!-- Hero 2 - Ad Optimization -->
+    <div class="snap-center shrink-0">
       <svg
         class="block sm:hidden w-full"
         xmlns="http://www.w3.org/2000/svg"
@@ -108,11 +106,78 @@
               <h2
                 class="text-3xl font-bold tracking-tight text-white sm:text-4xl"
               >
-                Monitor your analytics<br />24/7 using our dashboard.
+                Custom target locations<br />and ad spending limit.
               </h2>
               <p class="mt-6 text-lg leading-8 text-gray-300">
-                Manage your campaigns, track your performance, get insights, and
-                more using our dashboard tool.
+                You are in complete control of your campaign location targets
+                and budgeting. We have a variety of plans to suit the needs of
+                your business.
+              </p>
+              <div
+                class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start"
+              >
+                <a
+                  href="#"
+                  class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-dark-purple shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                  >See plans</a
+                >
+                <a href="#" class="text-sm font-semibold leading-6 text-white"
+                  >Learn more <span aria-hidden="true">→</span></a
+                >
+              </div>
+            </div>
+            <div class="flex justify-center lg:justify-end w-full h-full py-20">
+              <img
+                class="w-3/4 rounded-xl hover:scale-105 transition duration-500 ease-in-out shadow-xl"
+                src="https://static.vecteezy.com/system/resources/previews/010/925/603/original/marketing-campaign-business-strategy-digital-product-advertising-target-audience-in-social-media-brand-communication-company-website-flat-design-modern-illustration-vector.jpg"
+                alt="App screenshot"
+              />
+            </div>
+          </div>
+          <svg
+            class="block sm:hidden w-full"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1000 200"
+          >
+            <path
+              fill="#637EFE"
+              fill-opacity="1"
+              d="M0,96L40,101.3C80,107,160,117,240,106.7C320,96,400,64,480,53.3C560,43,640,53,720,80C800,107,880,149,960,149.3C1040,149,1120,107,1200,80C1280,53,1360,43,1400,37.3L1440,32L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
+            ></path>
+          </svg>
+        </div>
+      </div>
+    </div>
+    <!-- Hero 3 - Availability -->
+    <div class="snap-center shrink-0">
+      <svg
+        class="block sm:hidden w-full"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1100 250"
+      >
+        <path
+          fill="#637EFE"
+          fill-opacity="1"
+          d="M0,128L26.7,133.3C53.3,139,107,149,160,165.3C213.3,181,267,203,320,218.7C373.3,235,427,245,480,224C533.3,203,587,149,640,138.7C693.3,128,747,160,800,176C853.3,192,907,192,960,181.3C1013.3,171,1067,149,1120,149.3C1173.3,149,1227,171,1280,176C1333.3,181,1387,171,1413,165.3L1440,160L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"
+        ></path>
+      </svg>
+      <div class="bg-white sm:py-10 snap-center">
+        <div class="mx-auto sm:px-6 lg:px-8">
+          <div
+            class="relative isolate overflow-hidden bg-dark-purple px-6 pt-16 sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0"
+          >
+            <div
+              class="mx-auto max-w-md text-center lg:mx-0 lg: lg:py-32 lg:text-left"
+            >
+              <h2
+                class="text-3xl font-bold tracking-tight text-white sm:text-4xl"
+              >
+                Available anytime,<br />anywhere.
+              </h2>
+              <p class="mt-6 text-lg leading-8 text-gray-300">
+                We understand as a business owner that your time is valuable, so
+                our tools are built to be available on all devices, anytime,
+                anywhere.
               </p>
               <div
                 class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start"
@@ -130,7 +195,7 @@
             <div class="flex justify-center lg:justify-end w-full h-full py-20">
               <img
                 class="w-3/4 rounded-xl hover:scale-105 transition duration-500 ease-in-out shadow-xl"
-                src="@/assets/home/banner.png"
+                src="https://cdn.vox-cdn.com/thumbor/NqNRM6S4nYLA0Gi0yrajnvEal5Y=/1400x0/filters:no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/19234747/fortnite_firing_range.png"
                 alt="App screenshot"
               />
             </div>
