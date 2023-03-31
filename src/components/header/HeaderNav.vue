b91c0fdac4763c033ebfe80de5b408bfdfe0919e
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2023-03-31 00:44:08 -0500

    Refactor hover underline & retain line when on tab

--- src/components/header/HeaderNav.vue
+++ src/components/header/HeaderNav.vue
@@ -5,10 +5,18 @@
         v-for="(link, index) in links"
         :key="index"
         class="hover-underline-animation"
+        :class="[
+          link.to.name === currentRouteName
+            ? 'underline underline-offset-[6px] decoration-2 decoration-dark-purple'
+            : '',
+        ]"
         :title="link.title"
         :to="link.to"
         :sublinks="link.sublinks"
       >
+        <span
+          class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-dark-purple"
+        ></span>
       </HeaderNavItem>
       <button
         v-if="!userStore.isLoggedIn"
@@ -31,6 +39,8 @@
 
 <script setup lang="ts">
 import links, { ProfileLinks as profilelinks } from "@/links";
+import { computed } from "vue";
+import { useRoute } from "vue-router";
 import useUserStore from "@/stores/user";
 
 import HeaderNavItem from "./HeaderNavItem.vue";
@@ -39,6 +49,10 @@ const userStore = useUserStore();
 const getName = (): string => {
   return `${userStore.user?.first_name} ${userStore.user?.last_name}`;
 };
+
+const route = useRoute();
+const currentRouteName = computed(() => route.name);
+console.log(currentRouteName.value);
 </script>
 
 <style scoped></style>
