f5420096cd9b9821274b12737ed769f930227d30
VishwamR07 <vishwamraval@gmail.com>
2023-04-12 10:28:45 -0500

    linter fixes

--- src/views/pages/Services.vue
+++ src/views/pages/Services.vue
@@ -11,13 +11,12 @@
           >
           business
         </h1>
-       <p class="text-center pt-2 px-5 mb-10 mx-10">
-        Efficiently grow your business with our automated advertising services.
-        Advanced algorithms optimize campaigns for increased traffic and higher
-        conversion rates.
-      </p>
+        <p class="text-center pt-2 px-5 mb-10 mx-10">
+          Efficiently grow your business with our automated advertising
+          services. Advanced algorithms optimize campaigns for increased traffic
+          and higher conversion rates.
+        </p>
       </div>
-
     </div>
   </fade-in>
 
