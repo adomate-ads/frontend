6cc2b87ac131899c156a28173344ecde36a6c8de
VishwamR07 <vishwamraval@gmail.com>
2023-05-20 16:06:03 -0500

    added new team members

--- src/data/team.ts
+++ src/data/team.ts
@@ -40,6 +40,26 @@ const Members: Member[] = [
     linkedin: "https://www.linkedin.com/in/natestelling/",
     github: "https://github.com/NateStelling",
   },
+  {
+    name: "Vishwam Raval",
+    role: "Senior Developer",
+    email: "vishwam.raval@adomate.ai",
+    image:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png",
+    instagram: "https://www.instagram.com/vishwamr07/",
+    linkedin: "https://www.linkedin.com/in/vishwamr/",
+    github: "https://github.com/vishwamraval",
+  },
+  {
+    name: "James Nolan",
+    role: "Senior Developer",
+    email: "james.nolan@adomate.ai",
+    image:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png",
+    instagram: "https://www.instagram.com/jamesnolan2001/",
+    linkedin: "https://www.linkedin.com/in/jamesn8391/",
+    github: "https://github.com/jamesn8391",
+  },
 ];
 
 export default Members;
