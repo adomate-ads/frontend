3dfffaf478e5ec8b2bd2dc697dadb93c6fb95946
Raajheer1 <raaj.patel229@gmail.com>
2023-07-20 12:17:06 -0700

    Revert this to prod

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// const apiUrl =
-//   import.meta.env.VITE_API === "dev"
-//     ? "https://api.dev.adomate.ai/"
-//     : "https://api.adomate.ai/";
+const apiUrl =
+  import.meta.env.VITE_API === "dev"
+    ? "https://api.dev.adomate.ai/"
+    : "https://api.adomate.ai/";
 
-const apiUrl = "http://localhost:3000/";
+// const apiUrl = "http://localhost:3000/";
 
 export const API = axios.create({
   baseURL: apiUrl,
